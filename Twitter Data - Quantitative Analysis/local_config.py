9f5bfdc5181bf7c9cd49be41a65c955ab64fb450
Kavya N <kavya.laksh222@gmail.com>
2018-07-03 19:39:08 +0530

    first commit

---
+++ Twitter Data - Quantitative Analysis/local_config.py
@@ -0,0 +1,4 @@
+cons_tok = "2tw35h6NanpUy5jvwnLUN8CTa"
+cons_sec = "EA1mo6E8Crlt9y4wMZU17yn0KaG1dVyFYqJ5AYm4xibTmc3r7h"
+app_tok = "949862480783671296-TQTr5qyu4dGVJ9pGkJqIJvtmYk9X83x"
+app_sec = "WpEtLnpQOECTDWHsc5SF3I6cSAUcR4ic6XiMq1qsv1nQu"
